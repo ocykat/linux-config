a7532bec0d0ed842ebed9d8a5938623a1a7cb468
ocykat <nhatnguyen.sg.vn@gmail.com>
2019-06-04 12:25:15 +0700

    Add vim build scripts

---
+++ vim/scripts/pre.sh
@@ -0,0 +1,4 @@
+sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev \
+libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+python3-dev lua5.1 liblua5.1-dev libperl-dev git
